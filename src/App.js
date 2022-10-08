72b5a392762091e6d6bba7c5e4afb53f42b8628f
stephen azosike <stephenazosike@gmail.com>
2022-10-08 21:43:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import UseStates from './tutorial/1-useState/setup/2-useState-basics'
+import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
 
 function App() {
   return (
     <div className='container'>
-      <UseStates />
+      <UseStateArray />
     </div>
   )
 }
